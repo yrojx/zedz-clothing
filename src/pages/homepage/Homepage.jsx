6a277e66c05e51f93ababa3f487903dc755a5bf3
Yossan Sandi Rahmadi <yossansr06@gmail.com>
2020-01-12 12:17:21 +0800

    Create seperate components for homepage, Finished styling MenuItem component

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
+
+import Directory from '../../components/directory/Directory';
+
 import './Homepage.scss';
 
 const Homepage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHO NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
