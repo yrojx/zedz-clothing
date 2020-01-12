0b28fcfd3070932fdcb1a53f5b5ad649b6342bbf
Yossan Sandi Rahmadi <yossansr06@gmail.com>
2020-01-12 13:12:28 +0800

    Update navigating to MenuItem using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
