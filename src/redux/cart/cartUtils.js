bd68e81e69c5b4e0090ce8bad98409db15397d66
yossansr <yossansr06@gmail.com>
2020-03-28 00:26:56 +0800

    Add remove and increase/decrease function, also fix the cartUtils

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -4,16 +4,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   );
 
   if (existingCartItem) {
-    // First solution
-    // return cartItems.map(cartItem =>
-    //   cartItem.id === cartItemToAdd.id
-    //     ? { ...cartItem, quantity: cartItem.quantity + 1 }
-    //     : cartItem
-    // );
-    // Second solution
-    existingCartItem.quantity++;
-    return [...cartItems];
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
