687dc7faedd30570ddffd75720e1121a236548cd
Yossan Rahmadi <yossansr06@gmail.com>
2020-05-14 00:47:13 +0800

    Change to use get method (Promise pattern)

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -20,13 +20,13 @@ class Shop extends React.Component {
 		loading: true,
 	}
 
-	unsubscribeFromSnapshit = null
+	unsubscribeFromSnapshot = null
 
 	componentDidMount() {
 		const { updateCollections } = this.props
 		const collectionRef = firestore.collection('collections')
 
-		collectionRef.onSnapshot(async (snapshot) => {
+		collectionRef.get().then((snapshot) => {
 			const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
 			updateCollections(collectionsMap)
 			this.setState({ loading: false })
